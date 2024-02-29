98be5f1f3d5b9e2821e1cb522ded239ac0fe10bd
post-coder <gabrielspanu.dev@gmail.com>
2024-02-29 12:08:16 +0100

    feat: navbar completed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
+import AppNav from './components/AppNav.vue';
 export default {
-  
+  components: {
+    AppNav,
+  }
 }
 </script>
 
 <template>
-  
+  <AppNav></AppNav>
 </template>
 
 <style lang="scss">
+
+@use './styles/general' as *;
 
 </style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+    name: 'AppNav',
+
+    data() {
+        return {
+            links: ["Home", "Prodotti", "Chi Siamo", "Contatti"],
+
+            activeLinkIndex: 1,
+        }
+    }
+}
+</script>
+
+<template>
+  <nav>
+    <img id="logo" src="/img/la-molisana-logo.png" alt="Molisana Logo">
+
+    <ul id="links">
+
+        <li v-for="(currentLink, index) in links" :class="index == activeLinkIndex ? 'active' : ''">
+            {{ currentLink }}
+        </li> 
+
+    </ul>
+  </nav>        
+</template>
+
+<style lang="scss">
+
+nav {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+
+
+    #logo {
+        margin-bottom: 30px;
+    }
+
+    ul {
+        display: flex;
+        list-style-type: none;
+        padding: 0;
+
+        li {
+            padding: 10px 14px;
+            font-weight: 500;
+
+            cursor: pointer;
+
+            &.active {
+                background-color: #dae9fe;
+            }
+        }
+    }
+}
+
+
+</style>
